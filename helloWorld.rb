70af2a735c96665abb508ff4468627a0183e6d0c
HuoKnight <noman.gronckle@gmail.com>
2022-06-16 12:57:17 -0700

    commit whatever is here to wipe machine

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,1 +1,15 @@
-puts "Hello World!"
+print "Hello World!\n"
+
+a = 1 +
+2
+
+print a.to_s + "\n"
+
+
+#This is a cool print
+print <<"Tag you're it", <<"Yes you are."
+I'm it?
+Tag you're it
+Yes I am
+Yes you are.
+
